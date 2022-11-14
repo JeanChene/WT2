71523bd4f4cf94f72e75ba2d8450e9bc9bff2ee6
jimchen <jmcdevgit@gmail.com>
2022-11-14 13:58:02 +0100

    Comments

--- src/main/java/fr/jmc/Test.java
+++ src/main/java/fr/jmc/Test.java
@@ -1,5 +1,9 @@
+/**
+ * Commentaire
+ * */
 package fr.jmc;
 public class Test{
+    //----------------------
     public static void main(String[] args) {
         System.out.println("Hello"); 
     }
